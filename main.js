515cd383b5084b39e56abfa9cb4d0b2456bfce49
tmilne1 <tmilne1@gmail.com>
2019-12-01 22:00:18 -0500

    cleaned up code and completed

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-window.addEventListener('keydown', function (event){
+
+function playsound (event){
     const audio = this.document.querySelector(`audio[data-key = "${event.keyCode}"]`)
-    const key = this.document.querySelector(`.key[data-key = "${event.keyCode}"]`)
-    key.classList.add("playing")
+    const key = this.document.querySelector(`div[data-key = "${event.keyCode}"]`)
     if(!audio){return}
+
+    key.classList.add("playing")
+
     audio.currentTime = 0;
     audio.play();
+
     const keys = this.document.querySelectorAll(".key");
+    
     keys.forEach(item => item.addEventListener("transitionend", (e)=>{
         if (e.propertyName=="transform"){
             item.classList.remove("playing")
         }
     } ))
-}) 
+} 
+window.addEventListener('keydown', playsound)
