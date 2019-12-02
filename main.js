cdc797fcb6989e80d43a5d949a1e7a9b61e8fb85
tmilne1 <tmilne1@gmail.com>
2019-12-01 21:20:15 -0500

    added audio playback and key transformation

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 window.addEventListener('keydown', function (event){
     const audio = this.document.querySelector(`audio[data-key = "${event.keyCode}"]`)
+    const key = this.document.querySelector(`.key[data-key = "${event.keyCode}"]`)
+    key.classList.add("playing")
     if(!audio){return}
     audio.currentTime = 0;
     audio.play();
+    const keys = this.document.querySelectorAll(".key");
+    keys.forEach(item => item.addEventListener("transitionend", (e)=>{
+        if (e.propertyName=="transform"){
+            item.classList.remove("playing")
+        }
+    } ))
 }) 
